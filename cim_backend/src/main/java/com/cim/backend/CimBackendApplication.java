f112e676a0b5a2ca5d9d88fff82bc3574d026fc1
ShervinDani <rvshervin@gmil.com>
2025-06-01 21:18:07 +0530

    first commit

---
+++ cim_backend/src/main/java/com/cim/backend/CimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cim.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CimBackendApplication.class, args);
+	}
+
+}
---
+++ cim_backend/src/test/java/com/cim/backend/CimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cim.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
